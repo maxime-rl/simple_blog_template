d56fcb932338cb1c4d3ecfc8a947b169ae3809c2
Maxime Robil-Leprêtre <maxime.robil.leprtre@gmail.com>
2022-01-23 16:24:20 +0100

    feat: basic fetch data with getStaticProps

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -1,8 +1,35 @@
-export default function Home() {
+import { createClient } from "contentful";
+
+/**
+ * @name getStaticProps
+ * @returns {object} data response (articles)
+ */
+export async function getStaticProps() {
+  const client = createClient({
+    space: process.env.CONTENTFUL_SPACE_ID,
+    accessToken: process.env.CONTENTFUL_ACCESS_KEY,
+  });
+
+  const response = await client.getEntries({ content_type: "article" });
+
+  return {
+    props: {
+      articles: response.items,
+    },
+  };
+}
+
+export default function Home({ articles }) {
+  console.log(articles);
   return (
     <main className="min-h-[calc(100vh_-_7rem)] bg-slate-100">
       <section className="mx-auto p-3 max-w-screen-xl">
         <h1 className="text-4xl font-bold">Home page</h1>
+        {articles.map((article) => (
+          <div key={article.sys.id}>
+            <h2>{article.fields.title}</h2>
+          </div>
+        ))}
       </section>
     </main>
   );
