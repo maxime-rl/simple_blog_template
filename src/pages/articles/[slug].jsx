ae18c4d2064fc566c5961905c31b9e57484c0432
Maxime Robil-Leprêtre <maxime.robil.leprtre@gmail.com>
2022-01-27 19:24:19 +0100

    feat: add article page style
    an add @contentful/rich-text-react-renderer dependencie

--- src/pages/articles/[slug].jsx
+++ src/pages/articles/[slug].jsx
@@ -1,5 +1,7 @@
 import Head from "next/head";
 import { createClient } from "contentful";
+import Image from "next/image";
+import { documentToReactComponents } from "@contentful/rich-text-react-renderer";
 
 const client = createClient({
   space: process.env.CONTENTFUL_SPACE_ID,
@@ -40,15 +42,43 @@ export async function getStaticProps({ params }) {
  * @returns {JSX}
  */
 export default function ArticleDetails({ article }) {
-  console.log(article);
+  const { featuredImage, title, author, readingTime, tags, body } =
+    article.fields;
+
   return (
     <>
       <Head>
-        <title>{article.fields.title}</title>
+        <title>{title}</title>
       </Head>
-      <header>
-        <h1>{article.fields.title}</h1>
+      <header className="mx-auto xl:p-3 max-w-screen-xl">
+        <Image
+          src={`https:${featuredImage.fields.file.url}`}
+          width={featuredImage.fields.file.details.image.width}
+          height={featuredImage.fields.file.details.image.height}
+          alt={title}
+        />
+        <div className="mx-auto p-3 prose">
+          <h1 className="mb-3">{title}</h1>
+          <div className="text-xs font-light">
+            <span>By {author} - </span>
+            <span>{article.sys.createdAt.slice(0, 10)} - </span>
+            <span>{readingTime} mins read</span>
+          </div>
+          <div>
+            {tags.map((tag) => (
+              <span
+                className="px-1 mr-2 text-xs rounded bg-slate-300"
+                key={tag}
+              >
+                {tag}
+              </span>
+            ))}
+          </div>
+        </div>
       </header>
+      <main className="mx-auto p-3 prose">
+        {documentToReactComponents(body)}
+      </main>
     </>
   );
 }
