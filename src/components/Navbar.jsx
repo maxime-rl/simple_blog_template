c5fc1ab85b2ecaf3e62123af81d3c9ea2f8a600f
Maxime Robil-Leprêtre <maxime.robil.leprtre@gmail.com>
2022-04-30 17:54:13 +0200

    feat: add fields for hero and about page

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -2,18 +2,18 @@ import Link from "next/link";
 
 export default function Navbar() {
   return (
-    <nav className="relative shadow-md">
+    <nav className="fixed z-10 w-full bg-white shadow-md">
       <div className="flex justify-between mx-auto p-3 h-14 max-w-screen-xl">
-        <span className="text-2xl font-bold">B.</span>
-        <ul className="flex items-center gap-3">
+        <span className="text-2xl font-bold">Blog</span>
+        <ul className="flex items-center gap-8">
           <li>
             <Link href="/">
-              <a>Articles</a>
+              <a className="font-bold hover:underline">Articles</a>
             </Link>
           </li>
           <li>
             <Link href="/about">
-              <a>About</a>
+              <a className="font-bold hover:underline">About</a>
             </Link>
           </li>
         </ul>
--- src/pages/about.jsx
+++ src/pages/about.jsx
@@ -1,14 +1,53 @@
+import { createClient } from "contentful";
 import Head from "next/head";
+import Image from "next/image";
+import { documentToReactComponents } from "@contentful/rich-text-react-renderer";
 
-export default function about() {
+/**
+ * @name getStaticProps
+ * @returns {object} data response (about)
+ */
+export async function getStaticProps() {
+  const client = createClient({
+    space: process.env.CONTENTFUL_SPACE_ID,
+    accessToken: process.env.CONTENTFUL_ACCESS_KEY,
+  });
+
+  const response = await client.getEntries({ content_type: "about" });
+
+  return {
+    props: { about: response.items },
+    revalidate: 1,
+  };
+}
+
+export default function about({ about }) {
+  const { title, picture, description } = about[0].fields;
+
   return (
     <>
       <Head>
         <title>About</title>
       </Head>
-      <main className="min-h-[calc(100vh_-_7rem)] bg-slate-100">
-        <section className="mx-auto p-3 max-w-screen-xl">
-          <h1 className="text-4xl font-bold">About page</h1>
+      <main className="pt-14 min-h-[calc(100vh_-_3.5rem)] bg-slate-100">
+        <section className="mx-auto p-3 lg:py-20 max-w-screen-xl">
+          <h1 className="pt-3 lg:pt-0 pb-6 lg:pb-12 text-4xl font-bold">
+            {title}
+          </h1>
+          <div className="flex flex-col lg:flex-row lg:items-center gap-6 lg:gap-9">
+            <div className="relative lg:flex-1 w-full h-80 lg:h-96">
+              <Image
+                src={`https:${picture.fields.file.url}`}
+                layout="fill"
+                objectFit="cover"
+                className="rounded shadow-md"
+                alt={title}
+              />
+            </div>
+            <div className="lg:flex-1 w-full mx-auto prose">
+              {documentToReactComponents(description)}
+            </div>
+          </div>
         </section>
       </main>
     </>
